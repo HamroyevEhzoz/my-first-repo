4a9268d07874fb6b3768e528e95f87577c510f27
HamroyevEhzoz <hamroyevehzoz@gmail.com>
2022-06-02 10:49:52 +0500

    push files

---
+++ Valyuta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Application 
+{
+    public class Valyuta
+    {
+        public int id { get; set; }
+        public string Code { get; set; }
+        public string Ccy { get; set; }
+        public string CcyNm_RU { get; set; }
+        public string CcyNm_UZ { get; set; }
+        public string CcyNm_UZC { get; set; }
+        public string CcyNm_EN { get; set; }
+        public string Nominal { get; set; }
+        public string Rate { get; set; }
+        public string Diff { get; set; }
+        public string Date { get; set; }
+    }
+
+}
